d1c9bc5b51f4c29e2a187eeb3db7c765dd044dde
devdev20two <tobeteb524@edultry.com>
2021-03-05 20:40:09 +0300

    Final proto

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from collections import namedtuple
-
-from flask import Flask, render_template, redirect, url_for, request
+from sender import send
+from flask import Flask, render_template, redirect, url_for
+from flask import request as r
 
 app = Flask(__name__)
 
@@ -13,9 +14,10 @@ def main():
     return render_template('index.html')
 
 
-@app.route('/add_message', methods=['POST'])
-def add_message():
-    text = request.form['text']
-    tag = request.form['tag']
-    messages.append(Message(text, tag))
+@app.route('/request', methods=['POST'])
+def request():
+    url = r.form['url']
+    amount = r.form['amount']
+    coupon = r.form['coupon']
+    send(url, amount, coupon)
     return redirect(url_for('main'))
--- sender.py
+++ sender.py
@@ -2,8 +2,8 @@ import requests
 from cfg import token, group_id
 
 
-def send(url, amount):
+def send(url, amount, coupon):
     r = requests.get(
-        f'https://api.telegram.org/bot{token}/sendMessage?chat_id={group_id}&text=Плейлист: {url}\nКоличество: {amount}'
+        f'https://api.telegram.org/bot{token}/sendMessage?chat_id={group_id}&text=Плейлист: {url}\nКоличество: {amount}\nКупон: {coupon}'
     )
     return str(r.status_code)
